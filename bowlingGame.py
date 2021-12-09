9a81a37c48c3ff62b0dfd6b13c14ec6675440ca4
AvalieREINE <68837136+AvalieREINE@users.noreply.github.com>
2021-12-09 16:14:22 +1300

    make changes to pass all the unit test
    all tests passed

--- bowlingGame.py
+++ bowlingGame.py
@@ -1,24 +1,27 @@
 class BowlingGame:
     def __init__(self):
-        self.rolls = []
+        self.rolls = []  # each roll will have the number of pins knocked down
+        # [8, 10, 12, 8 ]
 
     def roll(self, pins):
-        self.rolls.append(pins)
+        self.rolls.append(pins)  # add number of pins to the roll
 
     def score(self):
         result = 0
         rollIndex = 0
-        for frameIndex in range(10):
-            if frameIndex in range(10):
-                result += self.StrikeScore(rollIndex)
+        for frameIndex in range(10):  # each game will have 10 frames
+            # made change to check if the first roll is a strike
+            # change StrikeScore to strikeScore to call the right function
+            if self.isStrike(rollIndex):
+                result += self.strikeScore(rollIndex)
                 rollIndex += 1
             elif self.isSpare(rollIndex):
                 result += self.spareScore(rollIndex)
                 rollIndex += 2
             else:
                 result += self.frameScore(rollIndex)
-            rollIndex += 2
-            return result
+                rollIndex += 2
+        return result  # change the indentation so that it returns the result at the end of the for loop
 
     def isStrike(self, rollIndex):
         return self.rolls[rollIndex] == 10
@@ -26,11 +29,11 @@ class BowlingGame:
     def isSpare(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex+1] == 10
 
-    def stickeScore(self, rollIndex):
+    def strikeScore(self, rollIndex):
         return 10 + self.rolls[rollIndex+1] + self.rolls[rollIndex+2]
 
     def spareScore(self, rollIndex):
         return 10 + self.rolls[rollIndex+2]
 
-    def frameScore(self, rollIndex): 
+    def frameScore(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
